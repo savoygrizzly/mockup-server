7994bf382f05939ea2f976d7603d44e7b0e95763
Mateo Laporte <mateo.laporte@prism-solutions.io>
2024-12-08 19:44:19 +0100

    initial
    Signed-off-by: Mateo Laporte <mateo.laporte@prism-solutions.io>

---
+++ server.py
@@ -0,0 +1,19 @@
+# save this as cors_server.py
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from functools import partial
+
+class CORSHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+def run(server_class=HTTPServer, handler_class=CORSHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
+
